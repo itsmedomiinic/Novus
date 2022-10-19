72f8113e780066c8e86e8a010e061408aed71944
Dominic Vega Ocampo <DominicVOcampo@gmail.com>
2022-10-19 14:08:41 +0100

    String1 excercises

---
+++ String1/String1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public String helloName(String name)
+        {
+            return "Hello " + name;
+        }
+
+        public String makeAbba(String a, String b)
+        {
+            return a + b + b + a;
+        }
+        public String makeTags(String tag, String word)
+        {
+            return $"<{tag}>{word}<{tag}>";
+        }
+
+        public String makeOutWord(String outer, String word)
+        {
+            String sub1, sub2;
+            sub1 = outer.Substring(0, 1);
+            sub2 = outer.Substring(2, 3);
+
+            return sub1 + word + sub2;
+        }
+    }
+}
