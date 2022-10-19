305a0f8d4f8d56dba8aafb517c40ec511ffd8df7
Dominic Vega Ocampo <DominicVOcampo@gmail.com>
2022-10-19 11:13:57 +0100

    Calculator

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program:Calculator
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, option, sum = 0;
+
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("Enter your first Number: ");
+            num1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter your second Number: ");
+            num2 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("What would you like to do? \n" +
+                "1. Add \n" +
+                "2. Subtract \n" +
+                "3. Multiply \n" +
+                "4. Divide \n" +
+                "5. QUIT \n" +
+                "Please enter an ineger: ");
+            option = Convert.ToInt16(Console.ReadLine());
+
+            if (option != 5)
+            {
+                switch (option)
+                {
+                    case 1:
+                        sum = calculator.Add(num1, num2);
+                        Console.WriteLine($"{num1} + {num2} = {sum}");
+                        break;
+                    case 2:
+                        sum = calculator.Subtract(num1, num2);
+                        Console.WriteLine($"{num1} - {num2} = {sum}");
+                        break;
+                    case 3:
+                        sum = calculator.Multiply(num1, num2);
+                        Console.WriteLine($"{num1} x {num2} = {sum}");
+                        break;
+                    case 4:
+                        sum = calculator.Divide(num1, num2);
+                        Console.WriteLine($"{num1} / {num2} = {sum}");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter an integer");
+            }
+            Console.ReadLine();
+        }
+    }
+}
