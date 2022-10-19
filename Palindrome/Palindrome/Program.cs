de3c1620169e6008763059715ca8e364ebc4fca7
Dominic Vega Ocampo <DominicVOcampo@gmail.com>
2022-10-19 14:43:27 +0100

    Problem 6 palindrome

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program:isPalindrome
+    {
+        static void Main(string[] args)
+        {
+            isPalindrome check = new isPalindrome();
+
+            Console.WriteLine("Enter a String: ");
+            String text = Console.ReadLine();
+
+            bool palin = check.palindromCheck(text);
+
+            if (palin == true)
+            {
+                Console.WriteLine("VALID PALINDROME");
+            }
+            else
+            {
+                Console.WriteLine("NOT A PALINDROME");
+            }
+            Console.ReadLine();
+        }
+
+        
+    }
+}
---
+++ Palindrome/Palindrome/isPalindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    public class isPalindrome
+    {
+        public bool palindromCheck(String text)
+        {
+            text.ToLower();
+            text.Replace(" ", "");
+
+            int j = text.Length;
+
+            for (int i = 0; i < j / 2; i++)
+            {
+                if (text[i] != text[j - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
