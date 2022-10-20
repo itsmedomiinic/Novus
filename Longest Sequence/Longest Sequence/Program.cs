751466ad84a20850aa688117e1930678051e130e
Dominic Vega Ocampo <DominicVOcampo@gmail.com>
2022-10-20 10:35:38 +0100

    problem 1

--- Longest Sequence/Longest Sequence/Program.cs
+++ Longest Sequence/Longest Sequence/Program.cs
@@ -10,7 +10,7 @@ namespace Longest_Sequence
     {
         static void Main(string[] args)
         {
-            int[] sales = { 1, 2, 1, 1, 0, 3, 1, 0, 0, 2, 4, 1, 0, 0, 0, 0, 2, 1, 0, 3, 1, 0, 0, 0, 6, 1, 3, 0, 0, 0 };
+            int[] sales = { 1, 2, 1, 1, 0, 3, 1, 0, 0, 2, 4, 1, 0, 0, 0, 0, 2, 1, 0, 3, 1, 0, 0, 0, 6, 1, 3, 0, 0, 0};
 
             int currentSequence = 0;
 
