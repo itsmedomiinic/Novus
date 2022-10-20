d41c99fb61f65a3a9f5114a581e291143cb0c383
Dominic Vega Ocampo <DominicVOcampo@gmail.com>
2022-10-20 11:01:58 +0100

    Problem 2 Anagrams

---
+++ Anagrams/Anagrams/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagrams
+{
+    class Program:sortStringClass
+    {
+        static void Main(string[] args)
+        {
+            sortStringClass sort = new sortStringClass();
+
+            String[] dictionary = { "parts", "traps", "arts", "rats", "starts", "tarts", "rat", "art", "tar", "tars", "stars", "stray" };
+            List<String> anagrams = new List<String>();
+
+            String target = sort.SortString("star");
+
+            String word = "";
+            int counter = 0;
+
+            for (int i = 0; i<dictionary.Length; i++)
+            {
+                word = sort.SortString(dictionary[i]);
+
+                if (word == target)
+                {
+                    anagrams.Add(dictionary[i]);
+                }
+            }
+
+            Console.WriteLine("The anagrams of 'star' are: ");
+            anagrams.ForEach(Console.WriteLine);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ Anagrams/Anagrams/sortStringClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagrams
+{
+    class sortStringClass
+    {
+        public string SortString(string input)
+        {
+            char[] characters = input.ToArray();
+            Array.Sort(characters);
+            return new string(characters);
+        }
+    }
+}
