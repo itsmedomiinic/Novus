d03d7670db504a8260d33b5711cec708933266b7
Dominic Vega Ocampo <DominicVOcampo@gmail.com>
2022-10-20 14:07:23 +0100

    Getting the hand of windows forms

---
+++ WinApp/WinApp/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinApp
+{
+    class Calculator
+    {
+        public int calc (int x)
+        {
+            return x * 5;
+        }
+    }
+}
