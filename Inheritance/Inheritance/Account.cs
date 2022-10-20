ab53276e2a49930196c805f48d51912a25eabbef
Dominic Vega Ocampo <DominicVOcampo@gmail.com>
2022-10-20 11:31:29 +0100

    Lesson 3 inheritance

---
+++ Inheritance/Inheritance/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Account:Person
+    {
+        //constructor
+        public Account() { }
+        public double getAccountBalance()
+        {
+            return currentAccount;
+        }
+        public double setAccountBalance(double amount)
+        {
+            currentAccount = currentAccount + amount;
+            return currentAccount;
+        }
+        public double withdrawFromAccount(double amountToWithdraw)
+        {
+            currentAccount = currentAccount - amountToWithdraw;
+            return currentAccount;
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate account class
+            Account myAccount = new Account();
+            double currentAmount = 0.0;
+
+            //save money in current account
+            currentAmount = myAccount.setAccountBalance(1000.0);
+            Console.WriteLine($"Current amount: {currentAmount}");
+
+            //withdraw 200.00
+            currentAmount = myAccount.withdrawFromAccount(200.00);
+            Console.WriteLine($"Current amount: {currentAmount}");
+
+            //get balance
+            currentAmount = myAccount.getAccountBalance();
+            Console.WriteLine($"Current amount: {currentAmount}");
+            Console.ReadLine();
+        }
+    }
+}
