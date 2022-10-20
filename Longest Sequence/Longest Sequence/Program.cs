5ee79f7ddb7346f3bb07aa655767e5fb97c9e5f6
Dominic Vega Ocampo <DominicVOcampo@gmail.com>
2022-10-20 10:34:34 +0100

    Problem 1 Longest Sequence

---
+++ Longest Sequence/Longest Sequence/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longest_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sales = { 1, 2, 1, 1, 0, 3, 1, 0, 0, 2, 4, 1, 0, 0, 0, 0, 2, 1, 0, 3, 1, 0, 0, 0, 6, 1, 3, 0, 0, 0 };
+
+            int currentSequence = 0;
+
+            int longestSequence = 0;
+
+            for (int i = 0; i<sales.Length; i++)
+            {
+                currentSequence++;
+
+                if (sales[i] != 0)
+                {
+                    currentSequence = 0;
+                }
+
+                if (currentSequence > longestSequence)
+                {
+                    longestSequence = currentSequence;
+                }
+            }
+
+            Console.WriteLine($"The longest sequence of days without a sale is {longestSequence}");
+            Console.ReadLine();
+        }
+    }
+}
