dcdfd81144d84516d0388fd83efb107149fa5b08
Dominic Vega Ocampo <DominicVOcampo@gmail.com>
2022-10-21 12:46:49 +0100

    Problem 5 String Reversal

---
+++ String Reversal/String Reversal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Reversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            String str = Console.ReadLine();
+            char[] chaArray = str.ToCharArray();
+
+            int i = 0;
+            int j = chaArray.Length-1;
+
+            while (i < j)
+            {
+                char temp = chaArray[i];
+                chaArray[i] = chaArray[j];
+                chaArray[j] = temp;
+
+                i++;
+                j--;
+            }
+
+            Console.WriteLine("The reverse string is: ");
+            foreach (char ch in chaArray)
+            {
+                Console.Write(ch);
+            }
+            Console.ReadLine();
+        }
+    }
+}
