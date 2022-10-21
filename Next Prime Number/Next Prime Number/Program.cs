8e3a7b7c705e00b057302e1a9c39e270508d1a9f
Dominic Vega Ocampo <DominicVOcampo@gmail.com>
2022-10-21 15:35:47 +0100

    Problem 9 Next Prime Number

---
+++ Next Prime Number/Next Prime Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Next_Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            choice++;
+
+            for (int i = 2; i < choice; i++)
+            {
+                if (choice % i == 0)
+                {
+                    choice++;
+                    i = 2;
+                }
+            }
+            Console.WriteLine($"The next prime number is {choice}");
+
+            Console.ReadLine();
+        }
+    }
+}
