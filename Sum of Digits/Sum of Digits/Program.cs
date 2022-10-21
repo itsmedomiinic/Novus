ed88759c98f3b06955aeeec234c77b722e9d404a
Dominic Vega Ocampo <DominicVOcampo@gmail.com>
2022-10-21 13:30:47 +0100

    Problem 7 Sum of Digits

---
+++ Sum of Digits/Sum of Digits/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sequence of integers: ");
+            int integ = Convert.ToInt32(Console.ReadLine());
+
+            
+            int sum = 0;
+            while (integ != 0)
+            {
+                sum += integ % 10;
+                integ /= 10;
+            }
+            
+
+            //int sum = integ.ToString().Sum(c => c - '0');
+
+            Console.WriteLine($"The sum is {sum}");
+            Console.ReadLine();
+        }
+    }
+}
