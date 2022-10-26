8bd59b39483dd17d9fd000fbfff9b71f39eafd53
Dominic Vega Ocampo <DominicVOcampo@gmail.com>
2022-10-26 15:02:22 +0100

    the areas program

--- The Areas Program/The Areas Program/Program.cs
+++ The Areas Program/The Areas Program/Program.cs
@@ -10,6 +10,24 @@ namespace The_Areas_Program
     {
         static void Main(string[] args)
         {
+            Circle c = new Circle();
+            Rectangle r = new Rectangle();
+            Triangle t = new Triangle();
+
+            double x = 10;
+            double y = 20;
+            double z = 30;
+
+            Console.WriteLine($"The area of the circle is: {c.Area(x)} ");
+            Console.WriteLine($"The perimeter of the circle is: {c.Perimeter(x)} ");
+
+            Console.WriteLine($"The area of the triangle is: {t.Area(x,y,z)} ");
+            Console.WriteLine($"The perimeter of the triangle is: {t.Perimeter(x,y,z)} ");
+
+            Console.WriteLine($"The area of the rectangle is: {r.Area(x,y)} ");
+            Console.WriteLine($"The perimeter of the rectangle is: {r.Perimeter(x,y)} ");
+
+            Console.ReadLine();
         }
     }
 }
