3f2ce235700a7412f561509860a071eb8ef5e227
Dominic Vega Ocampo <DominicVOcampo@gmail.com>
2022-10-26 10:07:13 +0100

    lesson 8 NUnit Testing

---
+++ MyTestProject/MyTestProject/CalcTest.cs
@@ -0,0 +1,34 @@
+﻿using
+System;
+using
+NUnit.Framework;
+namespace
+MyTestProject
+{
+    [TestFixture]
+    class
+    CalcTest
+    {
+        [Test]
+        public void AddTest()
+        {
+            Calc c = new Calc();
+            double x = 4.0;
+            double y = 5.0;
+            double expectedValue = 9.0;
+            double actualValue = c.Add(x, y);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [Test]
+        public void SubtractTest()
+        {
+            Calc c = new Calc();
+            double x = 10.0;
+            double y = 5.0;
+            double expectedValue = 5.0;
+            double actualValue = c.Subtract(x, y);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
